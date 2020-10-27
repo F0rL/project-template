b8eff2dc8b3cfdf8a8ae05e3ffad75bd3f5691a0
F0rL <kumaloveslife@gmail.com>
2020-10-27 13:43:19 +0800

    引入 base admin template

---
+++ admin-template/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tabWidth: 2, // // tab缩进大小,默认为2
+  semi: false, // 使用单引号, 默认false
+  singleQuote: true, // 行尾逗号,默认none,可选 none|es5|all
+  arrowParens: 'avoid' // 箭头函数参数括号 默认avoid 可选 avoid| always
+}
